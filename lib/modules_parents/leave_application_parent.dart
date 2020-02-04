85e97b66d43aa34a645f32cf69f5c4a460eb18e7
91814 <nikipatel385@gmail.com>
2020-02-04 11:17:23 +0700

    Initial commit

--- lib/modules_parents/leave_application_parent.dart
+++ lib/modules_parents/leave_application_parent.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class LeaveApplicationParent extends StatefulWidget {
@@ -6,11 +8,120 @@ class LeaveApplicationParent extends StatefulWidget {
 }
 
 class _LeaveApplicationParentState extends State<LeaveApplicationParent> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Leave Application'),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          autovalidate: true,
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        hintText: 'Topic'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter topic';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        hintText: 'From: dd/mm/yyyy'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter date';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        hintText: 'To: dd/mm/yyyy'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter date';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        hintText: 'Reason'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  height: 50.0,
+                  minWidth: 380.0,
+                  color: Theme.of(context).accentColor,
+                  textColor: Colors.white,
+                  child: new Text(
+                    "Submit",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () => {
+                    if (_formKey.currentState.validate())
+                      {
+                        print('valid application')
+                      }
+                  },
+                  splashColor: Colors.redAccent,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
