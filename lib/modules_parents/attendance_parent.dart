9c918b9dcf250a93226afa8023f92f255c5b66af
91814 <nikipatel385@gmail.com>
2020-02-04 12:46:51 +0700

    Initial commit

--- lib/modules_parents/attendance_parent.dart
+++ lib/modules_parents/attendance_parent.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'package:table_calendar/table_calendar.dart';
+
 class AttendanceParent extends StatefulWidget {
   @override
   _AttendanceParentState createState() => _AttendanceParentState();
 }
 
 class _AttendanceParentState extends State<AttendanceParent> {
+
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Check Attendance'),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(calendarController: _controller,)
+          ],
+        ),
+      )
     );
   }
 }
